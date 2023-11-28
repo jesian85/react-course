4399922aa712ae0ceaecd792e13db5f8dec2bed9
KornilovEL <kornilovel@nipigas.ru>
2023-11-28 11:02:40 +0300

    Задание №4 (исправления)

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,18 +1,17 @@
 import { Counter } from '../counter/component';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
 export const Dish = ({ dish }) => {
     const [counter, setCounter] = useState(0);
-    const decrease = useCallback(() => setCounter(c => c - 1), []);
-    const increase = useCallback(() => setCounter(c => c + 1), []);
     if (!dish) {
         return;
     }
-    // console.log(counter);
     return (
         <div>
             <span>{dish.name}</span>
-            <Counter value={counter} decrease={decrease} increase={increase} min={0} max={5} />
+            <Counter value={counter}
+                decrease={() => setCounter(c => c - 1)}
+                increase={() => setCounter(c => c + 1)} min={0} max={5} />
         </div>
     );
 };
--- src/components/review-form/component.jsx
+++ src/components/review-form/component.jsx
@@ -1,5 +1,5 @@
 import { Counter } from '../counter/component';
-import { useCallback, useEffect, useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 
 const SET_NAME = 'setName';
 const SET_TEXT = 'setText';
@@ -17,8 +17,8 @@ const DEFAULT_FORM_VALUE = {
 const reducer = (state, action) => {
     switch (action.type) {
         case SET_NAME:
-            return { 
-                ...state, 
+            return {
+                ...state,
                 name: action.payload,
                 text : DEFAULT_FORM_VALUE.text,
                 counter: DEFAULT_FORM_VALUE.counter,
@@ -26,9 +26,9 @@ const reducer = (state, action) => {
         case SET_TEXT:
             return { ...state, text: action.payload };
         case INCREASE_COUNTER:
-            return { ...state, counter: state.counter + action.payload };
+            return { ...state, counter: state.counter + COUNTER_STEP };
         case DECREASE_COUNTER:
-            return { ...state, counter: state.counter - action.payload };
+            return { ...state, counter: state.counter - COUNTER_STEP };
         case RESET_STATE:
             return DEFAULT_FORM_VALUE;
         default:
@@ -38,10 +38,7 @@ const reducer = (state, action) => {
 
 export const ReviewForm = ({ restaurantId }) => {
     const [formValue, dispatch] = useReducer(reducer, DEFAULT_FORM_VALUE);
-    const decrease = useCallback(() => dispatch({ type: DECREASE_COUNTER, payload: COUNTER_STEP }), []);
-    const increase = useCallback(() => dispatch({ type: INCREASE_COUNTER, payload: COUNTER_STEP }), []);
     useEffect(() => {
-        //console.log(restaurantId);
         dispatch({ type: RESET_STATE });
     }, [restaurantId]);
     return (
@@ -57,15 +54,16 @@ export const ReviewForm = ({ restaurantId }) => {
             <div>
                 <label htmlFor='text'>Текст</label>
                 <input
-                  id="text" 
+                  id="text"
                   type="text"
                   value={formValue.text}
                   onChange={(e) => dispatch({ type: SET_TEXT, payload: e.target.value })} />
             </div>
             <div>
-                <span>Рейтинг</span>
-                <Counter value={formValue.counter} decrease={decrease} increase={increase} min={1} max={5} />
-            </div>           
+                <Counter value={formValue.counter}
+                    decrease={() => dispatch({ type: DECREASE_COUNTER })}
+                    increase={() => dispatch({ type: INCREASE_COUNTER })} min={1} max={5} />
+            </div>
         </div>
     );
 };
